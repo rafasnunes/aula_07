b93a0067fb67c897fefa328a57947be9dfea31ca
rafasnunes <rafasnunes@gmail.com>
2025-04-29 21:31:21 -0300

    Envio das atividades práticas 1 e 2

---
+++ aula_07/atividade_01.py
@@ -0,0 +1,28 @@
+# Atividade Prática
+
+# Uma gráfica precisa de um programa que, dado um número, calcule o dobro, o triplo e o quadrado desse número para agilizar seus orçamentos.
+
+# O dobro da quantidade solicitada (para ofertas promocionais)
+# O triplo (para grandes eventos)
+# E a área total de um adesivo, considerando que o número informado é o lado do adesivo em centimetros.
+
+#Crie uma função que faça esses cálculos e mostre os resultados na tela.
+
+# Requisito
+
+# Criar uma função que receba um número
+# Calcular o dobro, triplo e quadrado
+# Retornar estes três valores para serem apresentados no programa principal.
+
+def calculo(n):  
+    d = n * 2
+    t = n * 3
+    q = n ** 2
+
+    return d, t, q
+
+num = int(input('Insira o número para o calculo: '))
+
+calculo1, calculo2, calculo3 = calculo(num)
+
+print(f'O dobro do número é {calculo1}. O triplo do número é {calculo2}. O número ao quadrado é {calculo3}')
